d4e8efbb6df2dc37b025745ffc5a30a2b64f2e57
Nathan <nathansherburn@gmail.com>
2014-11-23 15:18:40 +1100

    Add basic CAS support

--- server.js
+++ server.js
@@ -2,12 +2,33 @@ var express = require('express');
 var app = express();
 var units = require('./routes/units');
 
+var CAS = require('cas');
+var cas = new CAS({base_url: 'https://my.monash.edu.au/authentication/cas', service: 'http://opendata.eng.monash.edu'});
+
+var casLogin = function(req, res) {
+  var ticket = req.param('ticket');
+  if (ticket) {
+    cas.validate(ticket, function(err, status, username) {
+      if (err) {
+        // Handle the error
+        res.send({error: err});
+      } else {
+        // Log the user in
+        res.send({status: status, username: username});
+				next();
+      }
+		});
+	} else {
+		res.redirect('https://my.monash.edu.au/authentication/cas/login?service=http://opendata.eng.monash.edu/');
+	}
+};
+
 
 // Serve all static files in 'public' folder to users
-app.use(express.static(__dirname + '/public'));
+app.use('/', casLogin, express.static(__dirname + '/public'));
 
 // app.get('/unit/:id', units.findById);
-app.get('/units', units.getUnitList);
+app.get('/units', casLogin, units.getUnitList);
 
 
 
